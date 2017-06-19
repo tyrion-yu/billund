664c3e3845695a4dbc62eb8fd8cc4c2b94ebbd78
robinleej <robinin1@163.com>
2017-06-19 20:22:17 +0800

    add findIndex polyfill

--- packages/billund-supportor/lib/common/polyfill.js
+++ packages/billund-supportor/lib/common/polyfill.js
@@ -29,4 +29,34 @@ function arrayFind() {
     };
 }
 
+function arrayFindIndex() {
+    if (!Array.prototype.findIndex) {
+        Object.defineProperty(Array.prototype, 'findIndex', {
+            value(predicate) {
+                if (this == null) {
+                    throw new TypeError('"this" is null or not defined');
+                }
+
+                const o = Object(this);
+                const len = o.length >>> 0;
+
+                if (typeof predicate !== 'function') {
+                    throw new TypeError('predicate must be a function');
+                }
+                const thisArg = arguments[1];
+                let k = 0;
+                while (k < len) {
+                    const kValue = o[k];
+                    if (predicate.call(thisArg, kValue, k, o)) {
+                        return k;
+                    }
+                    k++;
+                }
+                return -1;
+            }
+        });
+    }
+}
+
 arrayFind();
+arrayFindIndex();
