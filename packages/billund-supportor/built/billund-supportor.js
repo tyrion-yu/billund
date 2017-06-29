1d18ce2ee1ecffa4d228454fbe76df8a7ec29f78
robinleej <robinin1@163.com>
2017-06-29 15:25:38 +0800

    add

--- packages/billund-supportor/built/billund-supportor.js
+++ packages/billund-supportor/built/billund-supportor.js
@@ -3505,7 +3505,37 @@ function arrayFind() {
     };
 }
 
+function arrayFindIndex() {
+    if (!Array.prototype.findIndex) {
+        Object.defineProperty(Array.prototype, 'findIndex', {
+            value: function value(predicate) {
+                if (this == null) {
+                    throw new TypeError('"this" is null or not defined');
+                }
+
+                var o = Object(this);
+                var len = o.length >>> 0;
+
+                if (typeof predicate !== 'function') {
+                    throw new TypeError('predicate must be a function');
+                }
+                var thisArg = arguments[1];
+                var k = 0;
+                while (k < len) {
+                    var kValue = o[k];
+                    if (predicate.call(thisArg, kValue, k, o)) {
+                        return k;
+                    }
+                    k++;
+                }
+                return -1;
+            }
+        });
+    }
+}
+
 arrayFind();
+arrayFindIndex();
 
 /***/ }),
 /* 14 */
